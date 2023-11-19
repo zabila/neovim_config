3474a0b65ef0451af7e67648ff9f5c8bcc70152b
Volodymyr Zabila <volodymyr.zabila@px.com>
2023-11-19 22:29:49 +0200

    Update cmake-tools. Updated config and keymaping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,10 @@ keymap.set("n", "<leader>uS", ":SymbolsOutline<CR>", { desc = "SymbolsOutline To
 --ChatGPT
 keymap.set("n", "<leader>Gg", "<cmd>ChatGPT<CR>", { desc = "ChatGPT Toggle" })
 keymap.set("n", "<leader>Ge", "<cmd>ChatGPTEditWithInstruction<CR>", { desc = "ChatGPT Edit with instruction" })
+
+--CMake-tools
+keymap.set("n", "<leader>Cb", "<cmd>CMakeBuild<CR>", { desc = "CMake Build" })
+keymap.set("n", "<leader>Cc", "<cmd>CMakeClean<CR>", { desc = "CMake Clean" })
+keymap.set("n", "<leader>Cd", "<cmd>CMakeDeleteCache<CR>", { desc = "CMake Delete Cache" })
+keymap.set("n", "<leader>Cg", "<cmd>CMakeGenerate<CR>", { desc = "CMake Generate" })
+keymap.set("n", "<leader>Cr", "<cmd>CMakeRun<CR>", { desc = "CMake Run" })
--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,7 +1,23 @@
 return {
 	"Civitasv/cmake-tools.nvim",
 	opts = {
-		cmake_kits_path = "C:/Users/wzabi/AppData/Local/CmakeTools/cmake-tools-kits.json",
+		cmake_regenerate_on_save = true,
+		cmake_soft_link_compile_commands = true,
+		cmake_compile_commands_from_lsp = false,
+
+		cmake_kits_path = function()
+			local osName = os.getenv("OS")
+			local currentUser = os.getenv("USER") or os.getenv("USERNAME")
+			if osName and osName:match("Windows") then
+				return "C:/Users/" .. currentUser .. "/AppData/Local/CmakeTools/cmake-tools-kits.json"
+			end
+		end,
+
+		cmake_generate_options = {
+			"-DCMAKE_EXPORT_COMPILE_COMMANDS=1",
+			"-DCMAKE_TOOLCHAIN_FILE=C:/Users/wzabi/scoop/apps/vcpkg/current/scripts/buildsystems/vcpkg.cmake",
+		},
+		cmake_build_directory = "build-${variant:buildType}",
 	},
 	event = "LazyFile",
 }
